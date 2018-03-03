3ce7cf7396674e461f7e05bfcf25801553602d92
Charlie J <charliefjennings+git@gmail.com>
2018-03-03 11:46:48 +0000

    Fixing typo.

--- charlie-fyi/src/Components/Intro.js
+++ charlie-fyi/src/Components/Intro.js
@@ -6,7 +6,7 @@ class Intro extends Component {
     render() {
         return(
           <section className="Container Intro Text-Center PadTopLarge MarTopLarge">
-            <h2>This is learning excersise.</h2>
+            <h2>This is learning excercise.</h2>
             <h3>I&apos;m learning <a href="https://reactjs.org/">React</a> while building myself a website.</h3>
             <small>For now, here&apos;s a few of my favourite photos from past few years.</small>
           </section>
